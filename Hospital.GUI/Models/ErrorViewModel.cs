36fc85d49a5276912232baa36bd9879cd028bec3
Nesey <vutungtam3110@gmail.com>
2024-10-02 09:57:46 +0700

    a

--- Hospital.GUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Hospital.GUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
